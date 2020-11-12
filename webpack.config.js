593bdcf8d708e1259a5f230a5e973e458ea3bde1
Chris Ahern <ahern.christopher@gmail.com>
2020-11-11 21:01:53 -0500

    Add initial files for Recipe App and some UI setup

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+module.exports = {
+    entry: ['babel-polyfill', './src/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'Public/Scripts'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env'],
+                    plugins: ['transform-object-rest-spread']
+                }
+            }
+        }]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        publicPath: '/Scripts/'
+    },
+    devtool: 'source-map'
+}
